812fffcdb87875d18aa71540677771b7e52e9e8b
Jean-Michel Bea <code@jm-bea.net>
2024-12-01 09:32:53 +0000

    Solve day 1 part 1

--- day_01/solution_01.ml
+++ day_01/solution_01.ml
@@ -1,27 +1,5 @@
-(* Hello, welcome to Advent of Code and OCaml! *)
-
 open ContainersLabels
 (* open Containers *)
-
-(* I prefer labeled arguments.
-
-   - This is `fold_left` with labeled argumnets, ~f and ~init are labels.
-     `List.fold_left ~f:( + ) ~init:0`
-     `List.fold_left ~init:0 ~f:( + )`
-
-   - Labeled arguments also have "punning", meaning that if `f` is defined
-     `let f = ( + )`
-     then we can omit the `:` part completly
-     `List.fold_left ~init:0` ~f`
-
-
-   - This is `fold_left` without labeled arguments, order matters, like you
-     are probably used to
-     `List.fold_left ( + ) 0`
-
-   See: https://ocaml.org/docs/labels
-
-   If you don't want this, open Containers instead of ContainersLabels. *)
 
 (* Advent of Code usually provides of with some smaller examples.
    I usually do inline testing to verify that my solution matches the examples.
@@ -30,41 +8,32 @@ open ContainersLabels
 
 (* Paste sample data here *)
 let sample = {|
-199
-200
-208
-210
-200
-207
-240
-269
-260
-263
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 |} |> String.trim
 
-(* If some parts of a solution is used in both Part_1 and Part_2 I place the
-   outside the modules *)
-
-(* These are my solutions to Day 1 from 2021: https://adventofcode.com/2021/day/1 *)
 module Part_1 = struct
-  let rec solve_aux accu l =
-    match l with
-    | [] | [ _ ] -> accu
-    | x1 :: x2 :: tl ->
-        let accu' = if x2 > x1 then succ accu else accu in
-        solve_aux accu' (x2 :: tl)
-
-  (* This needs to be a
-     `string -> int`
-     or
-     `string -> string *)
   let solve input =
-    let parsed = input |> String.lines |> List.map ~f:int_of_string in
-    solve_aux 0 parsed
+    let parser input =
+      Scanf.sscanf (String.trim input) "%i   %i" (fun a b -> (a, b))
+    in
+    let first, second =
+      input |> String.lines |> List.map ~f:parser |> List.split
+    in
+    let first = List.sort ~cmp:Int.compare first in
+    let second = List.sort ~cmp:Int.compare second in
+    let distances =
+      List.map2 ~f:(fun one two -> abs (one - two)) first second
+    in
+    List.fold_left ~f:Int.add ~init:0 distances
 
   (* According to the description the expected value should be 7 given the
      sample data. *)
-  let%test "sample data" = Test.(run int (solve sample) ~expect:7)
+  let%test "sample data" = Test.(run int (solve sample) ~expect:11)
 end
 
 module Part_2 = struct
@@ -89,7 +58,6 @@ end
 
 let run_1 () =
   Run.solve_int (module Part_1);
-  (* Run.solve_string (module Part_1); *)
   ()
 
 let run_2 () =
