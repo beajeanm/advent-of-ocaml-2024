4ec2a1ed37955b02dad6b7e3930337514f343c03
Jean-Michel Bea <code@jm-bea.net>
2024-12-02 08:17:16 +0000

    Solve day 2 part 1

--- day_02/solution_02.ml
+++ day_02/solution_02.ml
@@ -1,12 +1,55 @@
 open ContainersLabels
 
-let sample = {|
-...
-|} |> String.trim
+let sample =
+  {|
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+|}
+  |> String.trim
+
+let parse input =
+  String.lines input
+  |> List.map ~f:(String.split_on_char ~by:' ')
+  |> List.map ~f:(List.map ~f:Int.of_string_exn)
 
 module Part_1 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let is_gradual a b =
+    let diff = abs (a - b) in
+    diff >= 1 && diff <= 3
+
+  let is_safe_aux (state : int * [ `Decreasing | `Increasing ] * bool)
+      (current : int) =
+    match state with
+    | previous, `Increasing, _ ->
+        if is_gradual previous current && previous < current then
+          ((current, `Increasing, true), `Continue)
+        else ((current, `Increasing, false), `Stop)
+    | previous, `Decreasing, _ ->
+        if is_gradual previous current && previous > current then
+          ((current, `Decreasing, true), `Continue)
+        else ((current, `Decreasing, false), `Stop)
+
+  let is_safe = function
+    | [] | [ _ ] -> true
+    | first :: second :: tl ->
+        if is_gradual first second then
+          let init =
+            if first > second then (second, `Decreasing, true)
+            else (second, `Increasing, true)
+          in
+          let _, _, safeness = List.fold_while ~f:is_safe_aux ~init tl in
+          safeness
+        else false
+
+  let solve input =
+    let parsed = parse input in
+    List.filter ~f:is_safe parsed |> List.length
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:2)
 end
 
 module Part_2 = struct
@@ -15,7 +58,7 @@ module Part_2 = struct
 end
 
 let run_1 () =
-  (* Run.solve_int (module Part_1); *)
+  Run.solve_int (module Part_1);
   ()
 
 let run_2 () =
