d0ccd8b44d36216f301bc98a102efbaed35e2a27
Jean-Michel Bea <code@jm-bea.net>
2024-12-03 22:18:17 +0000

    Fancier day completed

--- completed_days.ml
+++ completed_days.ml
@@ -10,11 +10,13 @@ let result_for_day day =
   let path i = Printf.sprintf "day_%02d/p%d.expected" day i in
   let result =
     match (file_is_not_empty (path 1), file_is_not_empty (path 2)) with
-    | true, false -> "A: *\tB: -"
-    | true, true -> "A: *\tB: *"
-    | _ -> "A: -\tB: -"
+    | true, false -> "✅|❌"
+    | true, true -> "✅|✅"
+    | _ -> "❌|❌"
   in
-  Printf.sprintf "%i:\t%s" day result
+  Printf.sprintf "|%i|%s|" day result
 
 let () =
+  print_endline "|Day|Part 1|Part2|";
+  print_endline "|---|---|---|";
   List.(1 -- 25 >|= result_for_day) |> String.concat ~sep:"\n" |> print_endline
