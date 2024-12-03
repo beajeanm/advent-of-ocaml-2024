be82879de50c60a9d3f72bb63f264d281329a508
Jean-Michel Bea <code@jm-bea.net>
2024-12-03 07:11:04 +0000

    Solve day 3 part 2

--- day_03/solution_03.ml
+++ day_03/solution_03.ml
@@ -2,44 +2,67 @@ open ContainersLabels
 
 let sample =
   {|
-xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 |}
   |> String.trim
 
+type instruction = Do | Dont | Mul of int * int [@@deriving show]
+
 let is_digit = function '0' .. '9' -> true | _ -> false
 let int = Angstrom.(take_while is_digit >>| Int.of_string_exn)
-let mul = Angstrom.(string "mul(" *> both int (char ',' *> int) <* char ')')
+
+let mul_parser =
+  let open Angstrom in
+  let* _ = string "mul(" in
+  let* a = int in
+  let* _ = char ',' in
+  let* b = int in
+  let+ _ = char ')' in
+  Mul (a, b)
+
+let do_parser = Angstrom.(string "do()" >>| fun _ -> Do)
+let dont_parser = Angstrom.(string "don't()" >>| fun _ -> Dont)
+let parser = Angstrom.(mul_parser <|> do_parser <|> dont_parser)
 
 let parse input =
   let open Result in
   let parse_aux remaining_input =
-    Angstrom.parse_string ~consume:Angstrom.Consume.Prefix mul remaining_input
+    Angstrom.parse_string ~consume:Angstrom.Consume.Prefix parser
+      remaining_input
   in
-  let cut_to_next_mul str =
-    let len = String.length str in
-    let next_m = String.find ~start:1 ~sub:"mul(" str in
-    if next_m = -1 then "" else String.sub ~pos:next_m ~len:(len - next_m) str
-  in
+  let cut_str str = String.drop 1 str in
   let rec loop acc remaining_input =
     if String.is_empty remaining_input then List.rev acc
     else
       match parse_aux remaining_input with
-      | Ok tuple -> loop (tuple :: acc) (cut_to_next_mul remaining_input)
-      | Error _ -> loop acc (cut_to_next_mul remaining_input)
+      | Ok instruction -> loop (instruction :: acc) (cut_str remaining_input)
+      | Error _ -> loop acc (cut_str remaining_input)
   in
   loop [] input
 
 module Part_1 = struct
   let solve input =
-    let tuples = parse input in
-    List.map ~f:(fun (a, b) -> a * b) tuples |> Util.sum_list
+    let isntructions = parse input in
+    List.map
+      ~f:(function Do -> 0 | Dont -> 0 | Mul (a, b) -> a * b)
+      isntructions
+    |> Util.sum_list
 
   let%test "sample data" = Test.(run int (solve sample) ~expect:161)
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let process_instruction (enabled, acc) = function
+      | Do -> (true, acc)
+      | Dont -> (false, acc)
+      | Mul _ when not enabled -> (enabled, acc)
+      | Mul (a, b) -> (enabled, acc + (a * b))
+    in
+    let instructions = parse input in
+    List.fold_left ~init:(true, 0) ~f:process_instruction instructions |> snd
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:48)
 end
 
 let run_1 () =
@@ -47,5 +70,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
