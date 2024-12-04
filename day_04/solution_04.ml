97f9cadfaed89295116dd8f981b417cd9889f218
Jean-Michel Bea <code@jm-bea.net>
2024-12-04 15:24:40 +0000

    Solve day 4 part 2

--- day_04/solution_04.ml
+++ day_04/solution_04.ml
@@ -15,15 +15,30 @@ S.S.S.S.SS
 |}
   |> String.trim
 
-module Part_1 = struct
-  let to_grid line =
-    let chars =
-      String.lines_seq line |> Seq.map String.to_seq |> Seq.map Seq.to_array
-      |> Seq.to_array
-    in
-    let height = Array.length chars and width = Array.length chars.(0) in
-    Grid.init height width (fun (i, j) -> chars.(i).(j))
+let sample2 =
+  {|
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
+|}
+  |> String.trim
 
+let to_grid line =
+  let chars =
+    String.lines_seq line |> Seq.map String.to_seq |> Seq.map Seq.to_array
+    |> Seq.to_array
+  in
+  let height = Array.length chars and width = Array.length chars.(0) in
+  Grid.init height width (fun (i, j) -> chars.(i).(j))
+
+module Part_1 = struct
   let count_xmas grid =
     let rec find_word word position direction =
       if Grid.inside grid position then
@@ -56,8 +71,35 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let count_two_mas grid =
+    let find_mases position current acc =
+      if Char.equal current 'A' then
+        let nw = Grid.north_west position in
+        let ne = Grid.north_east position in
+        let se = Grid.south_east position in
+        let sw = Grid.south_west position in
+        let is_m = Char.equal 'M' in
+        let is_s = Char.equal 'S' in
+        let is_whithin_bounds =
+          Grid.inside grid nw && Grid.inside grid ne && Grid.inside grid se
+          && Grid.inside grid sw
+        in
+        let down_right_mas () =
+          (is_m (Grid.get grid nw) && is_s (Grid.get grid se))
+          || (is_m (Grid.get grid se) && is_s (Grid.get grid nw))
+        in
+        let up_left_mas () =
+          (is_m (Grid.get grid sw) && is_s (Grid.get grid ne))
+          || (is_m (Grid.get grid ne) && is_s (Grid.get grid sw))
+        in
+        if is_whithin_bounds && down_right_mas () && up_left_mas () then acc + 1
+        else acc
+      else acc
+    in
+    Grid.fold find_mases grid 0
+
+  let solve input = count_two_mas (to_grid input)
+  let%test "sample data" = Test.(run int (solve sample2) ~expect:9)
 end
 
 let run_1 () =
@@ -65,5 +107,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
