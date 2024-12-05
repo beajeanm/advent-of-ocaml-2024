059b6211696f863aed1c9cb41120bd4f4f56fb4d
Jean-Michel Bea <code@jm-bea.net>
2024-12-05 13:59:30 +0000

    Solve day 5 part 2

--- day_05/solution_05.ml
+++ day_05/solution_05.ml
@@ -49,32 +49,27 @@ module IntHash = Hashtbl.Make (Int)
 
 let build_hash_rules rules =
   let table = IntHash.create (List.length rules) in
-  List.iter
-    ~f:(fun (a, b) ->
-      let set =
-        IntHash.find_opt table b |> Option.value ~default:IntSet.empty
-      in
-      IntHash.replace table b (IntSet.add a set))
-    rules;
+  let update_table (a, b) =
+    let set = IntHash.find_opt table b |> Option.value ~default:IntSet.empty in
+    IntHash.replace table b (IntSet.add a set)
+  in
+  List.iter ~f:update_table rules;
   table
 
-module Part_1 = struct
-  let rec is_sorted rule_table = function
-    | [] -> true
-    | x :: xs ->
-        let predecessors =
-          IntHash.find_opt rule_table x |> Option.value ~default:IntSet.empty
-        in
-        let sorted_so_far =
-          List.find_opt ~f:(fun i -> IntSet.mem i predecessors) xs
-          |> Option.is_none
-        in
-        if sorted_so_far then is_sorted rule_table xs else false
+let rec is_sorted rules_table = function
+  | [] -> true
+  | x :: xs ->
+      let predecessors =
+        IntHash.find_opt rules_table x |> Option.value ~default:IntSet.empty
+      in
+      let sorted_so_far =
+        List.find_opt ~f:(fun i -> IntSet.mem i predecessors) xs
+        |> Option.is_none
+      in
+      if sorted_so_far then is_sorted rules_table xs else false
 
+module Part_1 = struct
   let solve input =
-    let filter_rule update =
-      List.filter ~f:(fun (a, b) -> List.mem a update && List.mem b update)
-    in
     let rules, updates = parse input in
     let rules_table = build_hash_rules rules in
     let valid_updates = List.filter ~f:(is_sorted rules_table) updates in
@@ -85,8 +80,37 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let ints = List.pp Int.pp
+
+  let is_in_the_right_place rules_table hd = function
+    | [] -> true
+    | xs ->
+        let predecessors =
+          IntHash.find_opt rules_table hd |> Option.value ~default:IntSet.empty
+        in
+        List.find_opt ~f:(fun i -> IntSet.mem i predecessors) xs
+        |> Option.is_none
+
+  let slow_sort rules_table update =
+    let rec aux acc = function
+      | [] -> acc
+      | x :: xs ->
+          if is_in_the_right_place rules_table x xs then aux (x :: acc) xs
+          else aux acc (List.append xs [ x ])
+    in
+    aux [] update |> List.rev
+
+  let solve input =
+    let rules, updates = parse input in
+    let rules_table = build_hash_rules rules in
+    let invalid_updates =
+      List.filter ~f:(fun l -> not (is_sorted rules_table l)) updates
+    in
+    let sorted_updates = List.map ~f:(slow_sort rules_table) invalid_updates in
+    List.map ~f:(fun l -> List.nth l (List.length l / 2)) sorted_updates
+    |> Util.sum
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:123)
 end
 
 let run_1 () =
@@ -94,5 +118,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
