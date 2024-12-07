a56b3d0b0ed5970f52a2dd5f61a66736c65f6021
Jean-Michel Bea <code@jm-bea.net>
2024-12-07 11:15:56 +0000

    Solve day 7 part 2

--- day_07/solution_07.ml
+++ day_07/solution_07.ml
@@ -28,17 +28,23 @@ let parse input =
   in
   String.lines input |> List.map ~f:parse_line
 
-let is_valid equation =
-  let tests =
-    List.fold_left
-      ~init:[ List.hd equation.numbers ]
-      ~f:(fun acc x ->
-        List.concat [ List.map ~f:(Int.add x) acc; List.map ~f:(Int.mul x) acc ])
-      (List.tl equation.numbers)
-  in
-  List.find_opt ~f:(Int.equal equation.total) tests |> Option.is_some
+let mull_acc = function [] -> [ 1 ] | acc -> acc
+let add_acc = function [] -> [ 0 ] | acc -> acc
 
 module Part_1 = struct
+  let is_valid equation =
+    let tests =
+      List.fold_left ~init:[]
+        ~f:(fun acc x ->
+          List.concat
+            [
+              List.map ~f:(Int.add x) (add_acc acc);
+              List.map ~f:(Int.mul x) (mull_acc acc);
+            ])
+        equation.numbers
+    in
+    List.find_opt ~f:(Int.equal equation.total) tests |> Option.is_some
+
   let solve input =
     let equations = parse input in
     List.filter ~f:is_valid equations
@@ -49,8 +55,35 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let concatenate a b = Int.of_string_exn (Int.to_string a ^ Int.to_string b)
+
+  let is_valid equation =
+    let rec loop acc numbers =
+      match numbers with
+      | [] -> acc
+      | hd :: tl ->
+          let results_concat =
+            let updated_acc =
+              if List.is_empty acc then [ hd ]
+              else List.map ~f:(fun n -> concatenate n hd) acc
+            in
+            loop updated_acc tl
+          in
+          let result_add = loop (List.map ~f:(Int.add hd) (add_acc acc)) tl in
+          let result_mul = loop (List.map ~f:(Int.mul hd) (mull_acc acc)) tl in
+          List.concat [ results_concat; result_add; result_mul ]
+    in
+    let tests = loop [] equation.numbers in
+
+    List.find_opt ~f:(Int.equal equation.total) tests |> Option.is_some
+
+  let solve input =
+    let equations = parse input in
+    List.filter ~f:is_valid equations
+    |> List.map ~f:(fun e -> e.total)
+    |> Util.sum
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:11387)
 end
 
 let run_1 () =
@@ -58,5 +91,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
