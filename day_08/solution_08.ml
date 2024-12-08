9e5af9aee84b8b6ef4779326e20689e5f42a63b6
Jean-Michel Bea <code@jm-bea.net>
2024-12-08 20:23:13 +0000

    Solve day 8 part 2

--- day_08/solution_08.ml
+++ day_08/solution_08.ml
@@ -51,39 +51,39 @@ let find_antennas grid =
   Grid.iter (add_antenna antennas) grid;
   antennas
 
-module Part_1 = struct
-  let find_antinodes grid antennas =
-    let aux (x1, y1) (x2, y2) =
-      let x_diff = x1 - x2 in
-      let y_diff = y1 - y2 in
-      [
-        (x1 - x_diff, y1 - y_diff);
-        (x1 + x_diff, y1 + y_diff);
-        (x2 - x_diff, y2 - y_diff);
-        (x2 + x_diff, y2 + y_diff);
-      ]
-      |> List.filter ~f:(Grid.inside grid)
-      |> List.filter ~f:(fun p ->
-             Point.(not (equal p (x1, y1) || equal p (x2, y2))))
-    in
-    let rec loop acc = function
-      | [] | [ _ ] -> acc
-      | p :: ps ->
-          let acc =
-            PointSet.add_list acc (List.map ~f:(aux p) ps |> List.concat)
-          in
-          loop acc ps
-    in
-    let results = loop PointSet.empty antennas in
-    let display_grid = Grid.copy grid in
-    PointSet.iter (fun p -> Grid.set display_grid p '#') results;
-    results
+let find_antinodes grid steps exclude_antennas antennas =
+  let aux (x1, y1) (x2, y2) =
+    let x_diff = x1 - x2 in
+    let y_diff = y1 - y2 in
+    List.(1 -- steps)
+    |> List.flat_map ~f:(fun step ->
+           [
+             (x1 - (x_diff * step), y1 - (y_diff * step));
+             (x1 + (x_diff * step), y1 + (y_diff * step));
+             (x2 - (x_diff * step), y2 - (y_diff * step));
+             (x2 + (x_diff * step), y2 + (y_diff * step));
+           ])
+    |> List.filter ~f:(Grid.inside grid)
+    |> List.filter ~f:(fun p ->
+           Point.(
+             (not exclude_antennas) || not (equal p (x1, y1) || equal p (x2, y2))))
+  in
+  let rec loop acc = function
+    | [] | [ _ ] -> acc
+    | p :: ps ->
+        let acc =
+          PointSet.add_list acc (List.map ~f:(aux p) ps |> List.concat)
+        in
+        loop acc ps
+  in
+  loop PointSet.empty antennas
 
+module Part_1 = struct
   let solve input =
     let grid = Util.parse_as_grid input in
     let antennas = find_antennas grid in
     let antennas_list = CharMap.to_seq_values antennas in
-    Seq.map (find_antinodes grid) antennas_list
+    Seq.map (find_antinodes grid 1 true) antennas_list
     |> Seq.fold_left PointSet.union PointSet.empty
     |> PointSet.to_seq |> Seq.length
 
@@ -91,8 +91,18 @@ module Part_1 = struct
 end
 
 module Part_2 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let grid = Util.parse_as_grid input in
+    let antennas = find_antennas grid in
+    let antennas_list = CharMap.to_seq_values antennas in
+    let antinodes =
+      Seq.map (find_antinodes grid 50 false) antennas_list
+      |> Seq.fold_left PointSet.union PointSet.empty
+      |> PointSet.to_seq |> Seq.length
+    in
+    antinodes
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:34)
 end
 
 let run_1 () =
@@ -100,5 +110,5 @@ let run_1 () =
   ()
 
 let run_2 () =
-  (* Run.solve_int (module Part_2); *)
+  Run.solve_int (module Part_2);
   ()
