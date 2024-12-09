21f5082317f007bc321be50f6c804c6667b3f7fd
Jean-Michel Bea <code@jm-bea.net>
2024-12-09 10:33:43 +0000

    Refactor to make part 2 easier

--- day_09/solution_09.ml
+++ day_09/solution_09.ml
@@ -4,67 +4,56 @@ let sample = {|
 2333133121414131402
 |} |> String.trim
 
-let parse input =
-  let files, spaces =
-    String.to_seq input
-    |> Seq.map (fun c -> Char.(code c - code '0'))
-    |> Seq.foldi
-         (fun (files, spaces) idx n ->
-           if idx mod 2 = 0 then (n :: files, spaces) else (files, n :: spaces))
-         ([], [])
-  in
-  (List.rev files, List.rev spaces)
-
 type file_block = { id : int; length : int }
 [@@deriving show { with_path = false }]
 
-let to_file_blocks = List.mapi ~f:(fun id length -> { id; length })
+type data = File of file_block | Space of int
+[@@deriving show { with_path = false }]
+
+let parse input =
+  String.to_seq input
+  |> Seq.map (fun c -> Char.(code c - code '0'))
+  |> Seq.foldi
+       (fun data idx n ->
+         if idx mod 2 = 0 then File { id = idx / 2; length = n } :: data
+         else Space n :: data)
+       []
+  |> List.rev
+
+let check_sum file_blocks =
+  List.map
+    ~f:(function
+      | File block -> List.init block.length ~f:(fun _ -> block.id)
+      | Space _ -> [])
+    file_blocks
+  |> List.concat
+  |> List.foldi ~init:0 ~f:(fun sum idx id -> sum + (idx * id))
 
 module Part_1 = struct
-  let move file_blocks spaces =
-    let cut_block block space =
-      if block.length = space then (block, [], None)
-      else if block.length > space then
-        ( { block with length = space },
-          [ { block with length = block.length - space } ],
-          None )
-      else (block, [], Some (space - block.length))
+  let compact (data_blocks : data CCDeque.t) =
+    let rec loop acc =
+      match CCDeque.take_front_opt data_blocks with
+      | Some (File _ as fb) -> loop (fb :: acc)
+      | Some (Space x as s) -> (
+          match CCDeque.take_back_opt data_blocks with
+          | Some (Space _) ->
+              CCDeque.push_front data_blocks s;
+              loop acc
+          | Some (File block as fb) ->
+              if block.length = x then loop (fb :: acc)
+              else if block.length > x then (
+                CCDeque.push_back data_blocks
+                  (File { block with length = block.length - x });
+                loop (File { block with length = x } :: acc))
+              else (
+                CCDeque.push_front data_blocks (Space (x - block.length));
+                loop (fb :: acc))
+          | None -> List.rev acc)
+      | None -> List.rev acc
     in
-    let rec loop acc = function
-      | [], _ -> List.rev acc
-      | [ block ], _ -> List.rev (block :: acc)
-      | blocks, [] -> List.concat [ List.rev acc; blocks ]
-      | blocks, s :: ss -> (
-          let len = List.length blocks in
-          let front, back = List.take_drop (len - 1) blocks in
-          let last_block = List.hd back in
-          let left_most, remaining_block, remaining_space =
-            cut_block last_block s
-          in
-          match remaining_space with
-          | None ->
-              loop
-                (List.hd front :: left_most :: acc)
-                (List.append (List.tl front) remaining_block, ss)
-          | Some x ->
-              loop (left_most :: acc)
-                (List.append front remaining_block, x :: ss))
-    in
-    loop [ List.hd file_blocks ] (List.tl file_blocks, spaces)
+    loop []
 
-  let check_sum file_blocks =
-    List.map
-      ~f:(fun block -> List.init block.length ~f:(fun _ -> block.id))
-      file_blocks
-    |> List.concat
-    |> List.foldi ~init:0 ~f:(fun sum idx id -> sum + (idx * id))
-
-  let solve input =
-    let files, spaces = parse input in
-    let file_blocks = to_file_blocks files in
-    let compacted = move file_blocks spaces in
-    check_sum compacted
-
+  let solve input = parse input |> CCDeque.of_list |> compact |> check_sum
   let%test "sample data" = Test.(run int (solve sample) ~expect:1928)
 end
 
