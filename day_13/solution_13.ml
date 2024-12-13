615b6bb5159009ed2780812446aaaa848122a29d
Jean-Michel Bea <code@jm-bea.net>
2024-12-13 09:35:59 +0000

    Solve day 13 part 1

--- day_13/solution_13.ml
+++ day_13/solution_13.ml
@@ -1,12 +1,67 @@
 open ContainersLabels
 
-let sample = {|
-...
-|} |> String.trim
+let sample =
+  {|
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+
+|}
+  |> String.trim
+
+type machine = { a : int * int; b : int * int; prize : int * int }
+[@@deriving show]
+
+let parse input =
+  let parse_machine remaining =
+    let[@warning "-8"] [ a_str; b_str; prize_str ], remaining =
+      List.take_drop 3 remaining
+    in
+    let a = Scanf.sscanf a_str "Button A: X+%i, Y+%i" Pair.make in
+    let b = Scanf.sscanf b_str "Button B: X+%i, Y+%i" Pair.make in
+    let prize = Scanf.sscanf prize_str "Prize: X=%i, Y=%i" Pair.make in
+    ({ a; b; prize }, List.tail_opt remaining |> Option.value ~default:[])
+  in
+  let rec loop (machines, remaining) =
+    if List.is_empty remaining then machines
+    else
+      let machine, remaining = parse_machine remaining in
+      loop (machine :: machines, remaining)
+  in
+  loop ([], String.lines input)
+
+let find_prize machine =
+  let ax, ay = machine.a in
+  let bx, by = machine.b in
+  let px, py = machine.prize in
+  let det = (ax * by) - (bx * ay) in
+  let a = ((px * by) - (py * bx)) / det in
+  let b = ((py * ax) - (px * ay)) / det in
+  let check_x = (a * ax) + (b * bx) in
+  let check_y = (a * ay) + (b * by) in
+  if check_x = px && check_y = py && a >= 0 && a <= 100 && b >= 0 && b <= 100
+  then (3 * a) + b
+  else 0
 
 module Part_1 = struct
-  let solve input = 0
-  let%test "sample data" = Test.(run int (solve sample) ~expect:0)
+  let solve input =
+    let machines = parse input in
+    let prizes = List.map ~f:find_prize machines in
+    prizes |> Util.sum
+
+  let%test "sample data" = Test.(run int (solve sample) ~expect:480)
 end
 
 module Part_2 = struct
@@ -15,7 +70,7 @@ module Part_2 = struct
 end
 
 let run_1 () =
-  (* Run.solve_int (module Part_1); *)
+  Run.solve_int (module Part_1);
   ()
 
 let run_2 () =
